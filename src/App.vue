26af06bf610c9a33b67967c496b5c93d5d7ebf39
Zora Chen <jj280385@gmail.com>
2022-02-22 17:36:17 +0800

    feat: user login page 建立

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,8 @@
 <style lang="scss">
 // 安裝字體
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap');
+@import './styles/reset.scss';
+@import './styles/font.scss';
+@import './styles/color.scss';
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
+import UserRegist from '../views/UserRegist.vue'
+import UserSetting from '../views/UserSetting.vue'
+import UserLogin from '../views/UserLogin.vue'
+import AdminLogin from '../views/AdminLogin.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'root',
+    component: UserLogin
+  },
+  {
+    path: '/regist',
+    name: 'user-regist',
+    component: UserRegist
+  },
+  {
+    path: '/login',
+    name: 'user-login',
+    component: UserLogin
+  },
+  {
+    path: '/setting',
+    name: 'user-setting',
+    component: UserSetting
+  },
+  {
+    path: '/admin',
+    name: 'admin-login',
+    component: AdminLogin
+  },
   {
     path: '*',
     name: 'not-found',
