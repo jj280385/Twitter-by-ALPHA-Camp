3fcba321b92fb454c1dde943c97a3b7547a68ddf
Zora Chen <jj280385@gmail.com>
2022-02-22 17:00:53 +0800

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <router-view />
+</template>
+
+<style lang="scss">
+// 安裝字體
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap');
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>404 Page Not Found</h1>
+  </main>
+</template>
