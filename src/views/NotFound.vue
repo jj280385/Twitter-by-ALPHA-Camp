e2bc88e0662bd26be1130139dd8d246a417cdb34
Pudding <>
2022-02-23 15:06:54 +0800

    Feat: 新增 Tweet Modal

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,15 @@
 <template>
   <main>
     <h1>404 Page Not Found</h1>
+    <TweetModal/>
   </main>
 </template>
+
+<script>
+import TweetModal from '../components/TweetModal.vue'
+export default {
+  components: {
+    TweetModal
+  }
+}
+</script>
