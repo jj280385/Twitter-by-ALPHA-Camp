2db325a28f50edd8d093a1297b244c82c5e0dd5c
Pudding <>
2022-02-25 00:57:37 +0800

    fix: View元件 css 屬性影響 Component 元件

--- src/views/AdminLogin.vue
+++ src/views/AdminLogin.vue
@@ -58,7 +58,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .container {
   margin: 60px;
   display: flex;
--- src/views/UserRegist.vue
+++ src/views/UserRegist.vue
@@ -97,7 +97,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .container {
   margin: 60px;
   display: flex;
