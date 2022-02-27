9861b1c6e902ee3291a1d318254946b0dab0e7b4
Pudding <>
2022-02-27 21:36:05 +0800

    feat: 建立吐司訊息元件、初始化 event bus

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 新定義一個vue實例，並建立 $bus prototype
+Vue.prototype.$bus = new Vue()
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
