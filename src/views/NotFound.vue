4068af34a1afe1ca0ce2d600480ed3e735a155af
Pudding <>
2022-03-02 05:01:16 +0800

    chore: 刪除無效程式碼

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,19 +1,15 @@
 <template>
   <main>
     <h1>404 Page Not Found</h1>
-    <ProfileModal/>
     <ReplyModal/>
-    <TweetModal/>
   </main>
 </template>
 
 <script>
-import TweetModal from '../components/TweetModal.vue'
 import ReplyModal from '../components/ReplyModal.vue'
-import ProfileModal from '../components/ProfileModal.vue'
 export default {
   components: {
-   TweetModal, ReplyModal,ProfileModal
+    ReplyModal
   }
 }
 </script>
