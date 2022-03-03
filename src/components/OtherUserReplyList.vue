ac5dc41704f70a8830eb7a123a7d19e19f45e531
Zora Chen <jj280385@gmail.com>
2022-03-03 22:43:37 +0800

    fix route again

--- src/components/OtherUserReplyList.vue
+++ src/components/OtherUserReplyList.vue
@@ -34,8 +34,8 @@
 </template>
 
 <script>
-import userAPI from "./../apis/user"
-import { fromNowFilter } from "./../utils/mixins";
+import userAPI from "../apis/user"
+import { fromNowFilter } from "../utils/mixins";
 
 export default {
   mixins: [fromNowFilter],
