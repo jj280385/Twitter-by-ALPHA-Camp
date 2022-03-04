f7581f9e26c1ce800da341ed01d5335e3c0800e6
Zora Chen <jj280385@gmail.com>
2022-03-04 09:48:40 +0800

    部屬測試

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,16 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Twitter-by-ALPHA-Camp/'
+    : '/' ,
   css: {
     loaderOptions: {
       sass: {
         prependData: `
-           @import "./src/styles/variables.scss";
+          @import "./src/styles/variables.scss";
         `
       }
     }
   }
 }
+
+
